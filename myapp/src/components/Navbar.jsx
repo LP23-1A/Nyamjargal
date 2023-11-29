09c2cfe82fb875bc93e865c094bc03c1cdd6be41
NyamjargalMTMN <metamancode@gmail.com>
2023-11-29 10:57:45 +0800

    darkmode

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,10 +1,29 @@
 import Sun from "../Utilities/images/Sun-img.jsx";
+import { useTheme } from "next-themes";
+import { useState } from "react";
 
 export default function navbar() {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+
+  const menuHandler = () => {
+      setIsOpen(!isOpen)
+  }
+
+  const themeToggle = () => {
+      if (theme == 'dark') {
+          setTheme('light')
+      }
+      if (theme == 'light' || theme === 'system') {
+          setTheme('dark')
+      }
+  }
   return (
     <div className="navbar flex justify-between my-[16px] mx-[80px] px-[32px]">
       <div className="logo text-[30px] font-bold">
-        <p>MYLOGO</p>
+        <p>Logo</p>
       </div>
       <div className="navbar-menu flex items-center font-medium">
         <ul className="header--list--container flex">
@@ -12,7 +31,7 @@ export default function navbar() {
             return <li key={"key-" + index}>{navItem}</li>;
           })}
         </ul>
-        <button className="header--theme">
+        <button className="header--theme" onClick={themeToggle}>
           <Sun />
         </button>
         <div className="header--cv--button"> Download CV</div>
