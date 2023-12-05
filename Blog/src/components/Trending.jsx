8edb6161a44eb253d023e9337b0ce7e2c47bcfab
NyamjargalMTMN <metamancode@gmail.com>
2023-12-05 10:21:21 +0800

    trending done

--- Blog/src/components/Trending.jsx
+++ Blog/src/components/Trending.jsx
@@ -1,26 +1,22 @@
-import Image from "next/image";
-import mountain from "@/utilities/images/mountain.png"
-
-const styling = {
-    backgroundImage: "url('../utilities/images/mountain.png')",
-    width:"100%",
-    height:"100%",
-  }
-
-
-export default function (){
-    return (
-        <div>
-            <div style={{ backgroundImage: "../utilities/images/mountain.png"}}>
-
-            </div>
-            <div className="rounded-xl">
-            
-                <div>Technology</div>
-                <p>The Impact of Technology on the Workplace: How Technology is Changing</p>
-            </div>
-                   
+export default function (props) {
+  return (
+    <div className="flex h-[320px] w-[289px] bg-cover  rounded-[12px] bg-no-repeat justify-center items-end relative"
+      style={{ backgroundImage: `url(${props.url})` }} >
+      <div className="absolute z-30 flex-col w-[230px] pb-7">
+        <div className="flex">
+          <p className=" px-[10px] py-[4px] text-white rounded-[6px]  bg-[#4B6BFB] text-[14px] ">
+            {props.title}
+          </p>
         </div>
-
-    );
+        <p className="text-white font-bold text-[18px] leading-7 mt-4">
+          {props.desc}
+        </p>
+      </div>
+      <div className=" absolute w-[289px] h-[320px] rounded-xl bg-slate-700 opacity-50 z-20">
+        </div>
+    </div>
+  );
 }
+
+
+
