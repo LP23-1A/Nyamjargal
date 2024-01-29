1bb1bb1803ba05ee300813d489d6a2c9c5b73420
NyamjargalMTMN <metamancode@gmail.com>
2024-01-29 11:32:20 +0800

    Foo Delivery project init

--- FoodDelivery/client/src/app/page.tsx
+++ FoodDelivery/client/src/app/page.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { TextField } from "@mui/material";
+import Footer from "@/component/Footer";
 
 export default function Home() {
   return (
-    <Stack className="">
-         <Button variant="text">Text</Button>
-         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    </Stack>
+    <main>
+      <Footer/>
+    </main>
   );
 }
---
+++ FoodDelivery/client/src/component/Footer.tsx
@@ -0,0 +1,7 @@
+export default function (){
+    return (
+        <div className=" w-[1440px] h-[545px] bg-[#18BA51]">
+                   
+        </div>
+    );
+}
