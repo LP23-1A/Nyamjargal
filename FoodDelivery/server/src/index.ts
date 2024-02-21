14e700a231198bae95dd51b3eaae6a4778ebd385
NyamjargalMTMN <metamancode@gmail.com>
2024-02-21 11:16:50 +0800

    back

--- FoodDelivery/server/src/index.ts
+++ FoodDelivery/server/src/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDatabase } from "./utils/database";
 import { auth } from "./router/auth";
+import {order} from "./router/order"
+import { food } from "./router/food";
+import { category } from "./router/category";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 connectDatabase();
@@ -11,6 +14,9 @@ const start = () => {
   app.use(express.json());
   app.use(cors());
   app.use('/auth', auth);
+  app.use('/or',order);
+  app.use('/food',food);
+  app.use('/category',category);
 
   app.get("/", (_req, res) => {
     res.status(200).send({ success: true, msg: "Working" });
---
+++ FoodDelivery/server/src/model/Order.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from 'mongoose';
+
+const OrderSchema = new mongoose.Schema({
+    userid: {
+        type: Schema.Types.ObjectId ,
+        ref: 'users',
+    },
+  orderNumber: {
+    type: Number,
+  },
+  foods:[{
+    type: Schema.Types.ObjectId ,
+    ref: 'foods',
+  }],
+  totalPrice: {
+    type: Number,
+  },
+  process: {
+    type: String,
+    enum: ['waiting', 'progress', 'delivered'],
+    default: 'waiting',
+  },
+  createdDate: {
+    type: Date,
+  },
+  district: {
+    type: String,
+  },
+  Khoroo: {
+    type: String,
+  },
+  Apartment: {
+    type: String,
+  },
+  Payment: {
+    type: String,
+    enum:['notpaid', 'paid'],
+    default: 'notpaid',
+  }
+});
+
+const OrderModel = mongoose.model('order', OrderSchema);
+
+export { OrderModel };
